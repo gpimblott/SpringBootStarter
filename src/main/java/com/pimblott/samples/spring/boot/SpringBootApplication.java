442ecfd72819b2e969b60433e93b61a44d1c477a
Gordon Pimblott <gordon@pimblott.com>
2015-05-16 11:18:39 +0100

    Inititial push

---
+++ src/main/java/com/pimblott/samples/spring/boot/SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.pimblott.samples.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by Gordon on 12/05/2015.
+ */
+public class SpringBootApplication {
+
+    public static void main( String[] args) {
+        SpringApplication.run(Example.class, args);
+    }
+}
